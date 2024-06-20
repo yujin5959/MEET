aa9ee48a8a8cee257cc5f8cc833b4937da87b8e6
JANGJEEYEON <jjiyeon27@gmail.com>
2024-06-20 14:10:32 +0900

    feat : 관리자 페이지 개발

--- src/pages/admin.tsx
+++ src/pages/admin.tsx
@@ -1,8 +1,56 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 import List from "@/components/list";
 
-function Admin() {
-  return <List></List>;
-}
+const Admin = () => {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    fetchUserList();
+  }, []);
+
+  // 서버에서 유저 목록 가져오기
+  const fetchUserList = async () => {
+    try {
+      const token = localStorage.getItem("accessToken");
+      if (!token) {
+        throw new Error("로그인이 필요합니다.");
+      }
+      const response = await axios.get("http://54.180.29.36/member/list", {
+        headers: {
+          Authorization: `${token}`,
+        },
+      });
+      const transformedUsers = response.data.data.map((user: any) => {
+        if (user.previllege === "accepted") {
+          user.previllege = "accept";
+        } else if (user.previllege === "denied") {
+          user.previllege = "deny";
+        }
+        return user;
+      });
+      setUsers(transformedUsers);
+    } catch (error) {
+      console.error("유저 목록을 불러오는 중 오류가 발생했습니다:", error);
+    }
+  };
+
+  return (
+    <div
+      className="min-h-screen flex items-center justify-center"
+      style={{ backgroundColor: "#242424" }}
+    >
+      <div
+        className="p-8 rounded-lg shadow-md w-full max-w-md mx-4"
+        style={{ backgroundColor: "#3f3f3f" }}
+      >
+        <h1 className="text-2xl font-bold mb-6 text-center text-white">
+          User List
+        </h1>
+        <List users={users} />
+      </div>
+    </div>
+  );
+};
 
 export default Admin;
