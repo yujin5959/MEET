cb465d5dea11ee1fc6298b551bd9086d992dfcec
yujin5959 <jinjin17104@naver.com>
2024-08-30 00:32:11 +0900

    fix: img 파일 만들기

--- src/pages/JoinVotePage.tsx
+++ src/pages/JoinVotePage.tsx
@@ -2,8 +2,8 @@ import React, { useState, useEffect } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 import JoinVoteBefore from "../components/JoinVoteBefore";
 import JoinVoteAfter from "../components/JoinVoteAfter";
-import alarm from '../assets/alarm.png';
-import vote from '../assets/vote.png';
+import alarm from '../assets/img/alarm.png';
+import vote from '../assets/img/vote.png';
 import FooterNav from "../components/FooterNav";
 import { server } from "@/utils/axios";
 
