be76f2c28134450fe295fdf20fc378f06b64d882
김지훈 <87347699+jomulagy@users.noreply.github.com>
2024-08-30 22:50:17 +0900

    feat : 참여여부 투표 기능 개발

--- src/components/JoinVoteAfter.tsx
+++ src/components/JoinVoteAfter.tsx
@@ -1,45 +1,35 @@
 import React from "react";
+import { voteItem } from "@/types/JoinVote";
 
 const JoinVoteAfter = ({
-  votedStatus,
+  votedItemId,
   setIsVoted,
+  itemList
 }: {
-  votedStatus: string;
-  setIsVoted: React.Dispatch<React.SetStateAction<boolean>>;
+  votedItemId: string;
+  setIsVoted: React.Dispatch<React.SetStateAction<boolean>>
+  itemList: voteItem[]
 }) => {
-  // 하드코딩된 인원 수
-  const participationCounts = {
-    참여: 10, // 예시: 참여 인원 수
-    불참여: 5, // 예시: 불참여 인원 수
-  };
-
   // 다시 투표하기 눌렀을 때
   const handleButtonClick = () => {
     setIsVoted(false);
   };
-
+  
   return (
     <div className="space-y-4">
       {/* 선택된 항목 강조 표시 */}
       <div className="w-full bg-white p-6 rounded-[24px] space-y-2">
-        <div
-          className={`flex items-center justify-between p-4 rounded-lg ${
-            votedStatus === "참여" ? "bg-blue-200" : "bg-white"
-          }`}
-        >
-          <span className="font-semibold text-lg">참여</span>
-          <span className="text-md">{participationCounts.참여}</span>
-        </div>
-        <div
-          className={`flex items-center justify-between p-4 rounded-lg ${
-            votedStatus === "불참여" ? "bg-blue-200" : "bg-white"
-          }`}
-        >
-          <span className="font-semibold text-lg">불참여</span>
-          <span className="text-md">{participationCounts.불참여}</span>
-        </div>
+        {itemList.map((item) => (
+            <div
+            className={`flex items-center justify-between p-4 rounded-lg ${
+              votedItemId === item.id ? "bg-blue-200" : "bg-white"
+            }`}
+          >
+            <span className="font-semibold text-lg">{item.name}</span>
+            <span className="text-md">{item.memberList.length}</span>
+          </div>
+        ))}
       </div>
-
       {/* 다시 투표하기 버튼 */}
       <button
         onClick={handleButtonClick}
--- src/components/JoinVoteBefore.tsx
+++ src/components/JoinVoteBefore.tsx
@@ -1,49 +1,58 @@
+import { voteItem } from "@/types/JoinVote";
 import React, { useState } from "react";
+import { server } from "@/utils/axios";
 
 // 투표 전 컴포넌트
 const JoinVoteBefore = ({
   meetId,
   setIsVoted,
-  setVotedStatus,
+  setVotedItem,
+  itemList
 }: {
   meetId: string;
   setIsVoted: React.Dispatch<React.SetStateAction<boolean>>;
-  setVotedStatus: React.Dispatch<React.SetStateAction<string>>;
+  setVotedItem: React.Dispatch<React.SetStateAction<string>>;
+  itemList: voteItem[]
 }) => {
-  const [selectedVote, setSelectedVote] = useState<string>("");
+  const [participateId, setParticipateId] = useState<string>("");
 
   const handleVote = () => {
-    if (selectedVote) {
-      setVotedStatus(selectedVote);
-      setIsVoted(true);
+    if (participateId) {
+      server.put("/meet/participate", {
+        data: {
+          meetId: meetId,
+          participateVoteItemIdList: [participateId]
+        },
+      })
+      .then((response) => {
+        setIsVoted(true);
+        setVotedItem(participateId);
+      })
+      .catch((error) => {
+         
+      });
+      
     }
   };
-
+  
   return (
     <div className="space-y-4">
       <div className="w-full bg-white p-6 rounded-[24px] space-y-2">
-        <label className="flex items-center text-lg">
-          <input
-            type="radio"
-            name="vote"
-            value="참여"
-            checked={selectedVote === "참여"}
-            onChange={(e) => setSelectedVote(e.target.value)}
-            className="mr-3 w-5 h-5"
-          />
-          참여
-        </label>
-        <label className="flex items-center text-lg">
-          <input
-            type="radio"
-            name="vote"
-            value="불참여"
-            checked={selectedVote === "불참여"}
-            onChange={(e) => setSelectedVote(e.target.value)}
-            className="mr-3 w-5 h-5"
-          />
-          불참여
-        </label>
+        {itemList.map((item) => (
+          <label key = {item.id} className="flex items-center text-lg">
+            <input
+              key = {item.id}
+              id={item.id}
+              type="radio"
+              name="vote"
+              value={item.name}
+              checked={participateId === item.id}
+              onChange={(e) => setParticipateId(e.target.id)}
+              className="mr-3 w-5 h-5"
+            />
+            {item.name}
+          </label>
+        ))}
       </div>
       <button
         className="bg-[#FFE607] p-3 rounded-[24px] text-lg w-full font-bold"
