30dc9a23d45b70932c86f5b5aed3606902a79456
장지연 <jangjiyeon@jangjiyeon-ui-MacBookAir.local>
2024-09-07 00:19:54 +0900

    deploy : 배포 준비

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,21 +1,5 @@
 import React from "react";
-import { server } from "@/utils/axios";
 import FooterNav from "../components/FooterNav";
-
-const adminUrl = "/admin";
-const handleAdmin = () => {
-  server
-    .get("/member/previllege")
-    .then((response) => {
-      console.log(response.data);
-      if (response.code === "200" && response.data.previllege === "admin") {
-        window.location.href = adminUrl;
-      }
-    })
-    .catch((error) => {
-      console.error("권한 정보를 가져오는 중 오류 발생:", error);
-    });
-};
 
 export const Dashboard = () => {  
   window.location.href = '/meet/list'
